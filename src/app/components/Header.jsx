a10f2955414fb82ac5c25c20090e9912758dc7fc
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-09 09:35:42 +0200

    Refactor Header component: Update header styling and add logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,16 +4,19 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      {/* I'll be adding logo */}
-      <Link href="/" className="text-2xl font-extrabold group cursor-pointer">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-800 drop-shadow-md group-hover:from-blue-700 group-hover:to-blue-500">
-          Leaner
-        </span>
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500 drop-shadow-md group-hover:from-yellow-600 group-hover:to-orange-400">
-          App
-        </span>
-      </Link>
+    <header className="bg-gradient-to-r from-purple-600 to-blue-600 shadow-md">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+          {/* I'll be adding logo */}
+        <Link href="/" className="text-2xl font-extrabold group cursor-pointer">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-800 drop-shadow-md group-hover:from-blue-700 group-hover:to-blue-500">
+            Leaner
+          </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500 drop-shadow-md group-hover:from-yellow-600 group-hover:to-orange-400">
+            App
+          </span>
+        </Link>
+        
+      </div>
     </header>
   );
 }
