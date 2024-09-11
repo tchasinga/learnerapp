48c7e8606957ae592659aa5065583fa4890e4832
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-11 13:05:45 +0200

    Refactor user.model.js to add required fields for username and avatar

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -18,4 +18,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
-});
+  username :{
+    type: String,
+     require: true,  
+  },
+  avatart : {
+    type: String,
+     require: true,  
+  }
+},{timestamps: true});
+
+const User = mongoose.model("User", userSchema)
+export default User;
+
