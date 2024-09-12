e730ae81edd50915fc1400b71a2b727b3e08c23b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-12 17:14:17 +0200

    add

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// this is my model of code 
+// this is my mode 
 
 const User = mongoose.model("User", userSchema);
 export default User;
