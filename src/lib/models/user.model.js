f33577e4ba06f8d103fbf2fb11611e60fa91444a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-09-15 09:02:12 +0200

    Refactor user.model.js to remove unnecessary comment and include timestamps option in user schema

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,35 +1,35 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    clerkId: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      require: true,
-    },
-    firstName: {
-      type: String,
-      require: true,
-    },
-    lastName: {
-      type: String,
-      require: true,
-    },
-    username: {
-      type: String,
-      require: true,
-    },
-    avatart: {
-      type: String,
-      require: true,
-    },
-  },
-  { timestamps: true }
-);
+// const userSchema = new mongoose.Schema(
+//   {
+//     clerkId: {
+//       type: String,
+//       require: true,
+//       unique: true,
+//     },
+//     email: {
+//       type: String,
+//       require: true,
+//     },
+//     firstName: {
+//       type: String,
+//       require: true,
+//     },
+//     lastName: {
+//       type: String,
+//       require: true,
+//     },
+//     username: {
+//       type: String,
+//       require: true,
+//     },
+//     avatart: {
+//       type: String,
+//       require: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-const User = mongoose.model("User", userSchema);
-export default User;
+// const User = mongoose.model("User", userSchema);
+// export default User;
